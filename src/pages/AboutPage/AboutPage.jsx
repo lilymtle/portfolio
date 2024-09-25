9c9e42fd7e1a3a0adcc23907e991fbe04fb90294
Lily Le <lmlt8180@gmail.com>
2024-09-24 23:38:39 -0400

    create about me page and stylesheet, add boilerplate code

---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,12 @@
+// import styling
+import "./AboutPage.scss";
+
+export function AboutPage() {
+    return (
+        <section className="about">
+            <h2 className="about__subheader">
+                About Me
+            </h2>
+        </section>
+    );
+};
