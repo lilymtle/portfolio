4a20daae1c5062e520455af4db6f1876797114ca
Lily Le <lmlt8180@gmail.com>
2024-09-24 23:38:48 -0400

    import about page

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { Hero } from "./components/Hero/Hero";
 
+// import pages
+import { AboutPage } from "./pages/AboutPage/AboutPage";
+
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
       <Hero />
+      <AboutPage />
       <Footer />
     </BrowserRouter>
   )
