880269b011ad7c0be486958223c70f258b45ed5f
Lily Le <lmlt8180@gmail.com>
2024-09-24 22:54:20 -0400

    import footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import "./styles/global.scss";
 // import components
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Header />
+
+      <Footer />
     </BrowserRouter>
   )
 };
