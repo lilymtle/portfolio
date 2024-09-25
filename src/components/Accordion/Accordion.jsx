507a50c945524f0622fe4c303d53011c8029cd3d
Lily Le <lmlt8180@gmail.com>
2024-09-25 18:34:45 -0400

    create accordion component and add styling

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,57 @@
+// import styling
+import "./Accordion.scss";
+
+// import components
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+export default function AccordionUsage() {
+    return (
+        <div>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    Technical Skills
+                </AccordionSummary>
+                <AccordionDetails>
+                    <p className="accordion__text--bold">
+                        Languages:
+                    </p>
+                    <p className="accordion__text">
+                        HTML, CSS, SASS, JavaScript, React, Node.js, Express.js, Knex.js, and
+                        MySQL.
+                    </p>
+
+                    <p className="accordion__text--bold accordion__text--tools">
+                        Tools:
+                    </p>
+
+                    <p className="accordion__text">
+                        Firebase, Git, GitHub, Agile Methodologies, Scrum, Jira, Postman, Material UI,
+                        Microsoft Office, Articulate 360.
+                    </p>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2-content"
+                    id="panel2-header"
+                >
+                    Soft Skills
+                </AccordionSummary>
+                <AccordionDetails>
+                    Leadership, communication, collaboration, prioritization, organization, project management,
+                    time management, attention to detail, critical thinking, problem-solving, multi-tasking,
+                    education/teaching/mentoring, user-centered thinking, adaptability, documentation, reporting,
+                    and data analysis.
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+}
