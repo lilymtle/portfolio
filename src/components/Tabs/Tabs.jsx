59025dbfacda631725882e267f8e2592afb049b6
Lily Le <lmlt8180@gmail.com>
2024-09-25 18:34:59 -0400

    update tabs component and styling
    - import accordion

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -9,6 +9,7 @@ import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import AccordionUsage from "../Accordion/Accordion.jsx";
 
 function CustomTabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -141,20 +142,117 @@ export function BasicTabs() {
                     </li>
                 </ul>
             </CustomTabPanel>
+
+
             <CustomTabPanel value={value} index={1}>
-                Item Two
+
+                <AccordionUsage />
+
+                <p clasName="tab__text">
+                    In addition to practicing and improving my JavaScript, I would also like
+                    to learn the following languages:
+                </p>
+
+                <ul className="tab__list--languages">
+                    <li className="tab__item">
+                        <p className="tab__text">
+                            Typescript
+                        </p>
+                    </li>
+
+                    <li className="tab__item">
+                        <p className="tab__text">
+                            Python
+                        </p>
+                    </li>
+
+                    <li className="tab__item">
+                        <p className="tab__text">
+                            Next.js & Nest.js
+                        </p>
+                    </li>
+
+                    <li className="tab__item">
+                        <p className="tab__text">
+                            Flutter
+                        </p>
+                    </li>
+                </ul>
+                
             </CustomTabPanel>
+
+
             <CustomTabPanel value={value} index={2}>
                 <p className="tab__text">
-                    Get to know me as a <span className="italics">person</span>!
+                    Although understanding my education and skills are important,
+                    getting to know me as a <span className="italics">person</span> is also vital!
                 </p>
 
                 <ul className="tab__list">
-                    <li className="tab__item">
+                    <li className="tab__item--fun">
+                        <div className="tab__emoji">
+                            🐱
+                        </div>
                         <p className="tab__text">
-                            💉 
+                            I have two cats, Catrick (orange tabby) and Bib (black). 
                         </p>
                     </li>
+
+                    <li className="tab__item--fun">
+                        <div className="tab__emoji">
+                            🏋🏻‍♀️ 
+                        </div>
+                        <p className="tab__text">
+                            I love weightlifting/powerlifting, swimming, hiking, going on walks, Muay Thai, and Brazilian JiuJitsu. 
+                        </p>
+                    </li>
+
+                    <li className="tab__item--fun">
+                        <div className="tab__emoji">
+                            🎮 
+                        </div>
+                        <p className="tab__text">
+                            I am a PC and console gamer. I love games like ACNH and SDV!
+                        </p>
+                    </li>
+
+                    <li className="tab__item--fun">
+                        <div className="tab__emoji">
+                            👀
+                        </div>
+                        <p className="tab__text">
+                            I love anime and manga.
+                        </p>
+                    </li>
+
+                    <li className="tab__item--fun">
+                        <div className="tab__emoji">
+                            💬
+                        </div>
+                        <p className="tab__text">
+                            I am greedy for languages. I am learning Korean, Japanese, and French.
+                        </p>
+                    </li>
+
+                    <li className="tab__item--fun">
+                        <div className="tab__emoji">
+                            🍟
+                        </div>
+                        <p className="tab__text">
+                            I love to cook, bake, and discover new places to eat delicious food.
+                        </p>
+                    </li>
+
+                    <li className="tab__item--fun">
+                        <div className="tab__emoji">
+                            ✈️
+                        </div>
+                        <p className="tab__text">
+                            I've been to: Seoul, Busan, Hanoi, Saigon, Nha Trang, Da Nang, Hội An, 
+                            and more. I will be visiting England and Scotland in October 2024!
+                        </p>
+                    </li>
+
                 </ul>
             </CustomTabPanel>
         </Box>
