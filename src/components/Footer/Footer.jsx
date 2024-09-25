2370ac76c9c978df8ddfc4b1bd49b74c497d8bfb
Lily Le <lmlt8180@gmail.com>
2024-09-24 22:54:03 -0400

    create footer component and add styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+// import styling
+import "./Footer.scss";
+
+export function Footer() {
+    return (
+        <footer>
+            <p className="footer__text">
+                This portfolio was coded with love by me. 🩵
+            </p>
+        </footer>
+    );
+};
