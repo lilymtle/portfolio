f9dcfa7a7f4015dc8ee390f3b6b78768af80994c
Lily Le <lmlt8180@gmail.com>
2024-09-24 22:45:32 -0400

    add portfolio logo and import nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
+// import styling
+import { NavBar } from "../NavBar/NavBar";
+import "./Header.scss";
+
 export function Header() {
     return (
         <header>
-            Placeholder header
+            <img
+                className="portfolio-logo"
+                src="/src/assets/logos/portfolio-logo.png"
+                alt="portfolio logo" />
+
+            <NavBar />
         </header>
     );
 };
