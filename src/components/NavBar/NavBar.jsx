b6e5203c66b69d0ba454df8ae3b61d345ec90630
Lily Le <lmlt8180@gmail.com>
2024-09-24 22:45:50 -0400

    create navbar component and add styling

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,42 @@
+// import styling
+import "./NavBar.scss";
+
+// import component
+import { Link } from "react-router-dom";
+
+export function NavBar() {
+    return (
+        <section className="nav-bar">
+            <ul className="nav-bar__list">
+                <li className="nav-bar__item">
+                    <Link className="nav-bar__link" to="/">
+                        <p className="nav-bar__text">
+                            About
+                        </p>
+                    </Link>
+                </li>
+                <li className="nav-bar__item">
+                    <Link className="nav-bar__link" to="/">
+                        <p className="nav-bar__text">
+                            Project
+                        </p>
+                    </Link>
+                </li>
+                <li className="nav-bar__item">
+                    <Link className="nav-bar__link" to="/">
+                        <p className="nav-bar__text">
+                            Work
+                        </p>
+                    </Link>
+                </li>
+                <li className="nav-bar__item">
+                    <Link className="nav-bar__link" to="/">
+                        <p className="nav-bar__btn">
+                            Resume
+                        </p>
+                    </Link>
+                </li>
+            </ul>
+        </section>
+    );
+};
