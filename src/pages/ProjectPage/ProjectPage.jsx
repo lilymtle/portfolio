6c168dae2982e0311895f17dc820956c77fd2d4e
Lily Le <lmlt8180@gmail.com>
2024-09-25 20:32:49 -0400

    update classnames, add divs for flip card

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -3,19 +3,42 @@ import "./ProjectPage.scss";
 
 export function ProjectPage() {
     return (
-        <section className="project">
+        <section className="projects">
             <h2 className="project__subheader">
                 Projects
             </h2>
 
+            <p className="project__text">
+                Hover over the images for more details!
+            </p>
+
             <section className="project__cards">
                 <ul className="card__list">
                     <li className="card">
-                        <div className="card__overlay">
-                            <img
-                                className="card__img"
-                                src="src/assets/images/mood-compass-img.png"
-                                alt="screenshot of moodcompass project" />
+                    <div className="card__inner">
+                        <div className="card__front">
+                            <h3 className="project__subheader--small">
+                                MoodCompass
+                            </h3>
+
+                            <div className="card__img">
+                                <img className="card__img--laptop" 
+                            src="/src/assets/images/laptop-screen.png" />
+
+                            <div className="card__overlay">
+                                <img
+                                    className="card__img--project"
+                                    src="src/assets/images/mood-compass-img.png"
+                                    alt="screenshot of moodcompass project" />
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="card__back">
+                            <h3 className="project__subheader--small">
+                                Details
+                            </h3>
+                        </div>
                         </div>
                     </li>
                 </ul>
