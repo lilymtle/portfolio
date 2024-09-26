51cf4e4c12d614e4d5dbfa0c64f41373fe970446
Lily Le <lmlt8180@gmail.com>
2024-09-26 12:07:11 -0400

    add cards

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -41,6 +41,147 @@ export function ProjectPage() {
                         </div>
                         </div>
                     </li>
+
+                    <li className="card">
+                    <div className="card__inner">
+                        <div className="card__front">
+                            <h3 className="project__subheader--small">
+                                TELUS What Your Preference Is
+                            </h3>
+
+                            <div className="card__img">
+                                <img className="card__img--laptop" 
+                            src="/src/assets/images/laptop-screen.png" />
+
+                            <div className="card__overlay">
+                                <img
+                                    className="card__img--project"
+                                    src="src/assets/images/mood-compass-img.png"
+                                    alt="screenshot of moodcompass project" />
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="card__back">
+                            <h3 className="project__subheader--small">
+                                Casestudy
+                            </h3>
+                        </div>
+                        </div>
+                    </li>
+
+                    <li className="card">
+                    <div className="card__inner">
+                        <div className="card__front">
+                            <h3 className="project__subheader--small">
+                                InStock
+                            </h3>
+
+                            <div className="card__img">
+                                <img className="card__img--laptop" 
+                            src="/src/assets/images/laptop-screen.png" />
+
+                            <div className="card__overlay">
+                                <img
+                                    className="card__img--project"
+                                    src="src/assets/images/mood-compass-img.png"
+                                    alt="screenshot of moodcompass project" />
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="card__back">
+                            <h3 className="project__subheader--small">
+                                Casestudy
+                            </h3>
+                        </div>
+                        </div>
+                    </li>
+
+                    <li className="card">
+                    <div className="card__inner">
+                        <div className="card__front">
+                            <h3 className="project__subheader--small">
+                                BrainFlix
+                            </h3>
+
+                            <div className="card__img">
+                                <img className="card__img--laptop" 
+                            src="/src/assets/images/laptop-screen.png" />
+
+                            <div className="card__overlay">
+                                <img
+                                    className="card__img--project"
+                                    src="src/assets/images/mood-compass-img.png"
+                                    alt="screenshot of moodcompass project" />
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="card__back">
+                            <h3 className="project__subheader--small">
+                                Casestudy
+                            </h3>
+                        </div>
+                        </div>
+                    </li>
+
+                    <li className="card">
+                    <div className="card__inner">
+                        <div className="card__front">
+                            <h3 className="project__subheader--small">
+                                Bandsite
+                            </h3>
+
+                            <div className="card__img">
+                                <img className="card__img--laptop" 
+                            src="/src/assets/images/laptop-screen.png" />
+
+                            <div className="card__overlay">
+                                <img
+                                    className="card__img--project"
+                                    src="src/assets/images/mood-compass-img.png"
+                                    alt="screenshot of moodcompass project" />
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="card__back">
+                            <h3 className="project__subheader--small">
+                                Casestudy
+                            </h3>
+                        </div>
+                        </div>
+                    </li>
+
+                    <li className="card">
+                    <div className="card__inner">
+                        <div className="card__front">
+                            <h3 className="project__subheader--small">
+                                Coffee Shop
+                            </h3>
+
+                            <div className="card__img">
+                                <img className="card__img--laptop" 
+                            src="/src/assets/images/laptop-screen.png" />
+
+                            <div className="card__overlay">
+                                <img
+                                    className="card__img--project"
+                                    src="src/assets/images/mood-compass-img.png"
+                                    alt="screenshot of moodcompass project" />
+                            </div>
+                            </div>
+                        </div>
+
+                        <div className="card__back">
+                            <h3 className="project__subheader--small">
+                                Casestudy
+                            </h3>
+                        </div>
+                        </div>
+                    </li>
+
                 </ul>
             </section>
         </section>
