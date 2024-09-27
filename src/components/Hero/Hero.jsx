f7bbba875f3b3e5f8cce3dc1b738b2fdaca405b3
Lily Le <lmlt8180@gmail.com>
2024-09-27 19:20:35 -0400

    add new hero image and animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,21 @@
 // import styling
 import "./Hero.scss";
 
+import { Header } from "../Header/Header.jsx";
+
 export function Hero() {
     return (
         <main className="hero">
-            <div className="hero__container">
+            <div className="hero__background-container">
+                <img 
+                className="hero__img"
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" />
+                <div className="stars"></div>
+                <div className="twinkling"></div>
+                <div className="clouds"></div>
+            </div>
+
+            {/* <div className="hero__container">
                 <div className="hero__wrapper">
                     <h1 className="hero__header">
                         Hi there 👋🏻,
@@ -25,7 +36,7 @@ export function Hero() {
                         src="/src/assets/images/hero-img.svg"
                         alt="illustration of a woman on a laptop" />
                 </div>
-            </div>
+            </div> */}
         </main>
     );
 };
