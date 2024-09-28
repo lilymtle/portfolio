ee76923ba8946c1a4d14d0e9b208777a0e87f159
Lily Le <lmlt8180@gmail.com>
2024-09-27 20:27:34 -0400

    add about text

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -6,8 +6,16 @@ export function AboutPage() {
     return (
         <section className="about" id="about">
             <h2 className="about__subheader">
-                About Me
+                About
             </h2>
+
+            <p className="about__text">
+                As a former nurse transitioning to software engineering, I bring a unique 
+                perspective to tech development. My nursing experience has shaped my understanding
+                of user needs, allowing me to create empathetic, efficient software solutions.
+                I also honed my ability to communicate effectively, manage high-stress situations,
+                and think critically—skills that seamlessly translate to software development.
+            </p>
             
             <BasicTabs />
         </section>
