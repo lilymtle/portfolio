7ecd64223f671a98ff1ccf8e4d87e04c7567ef5a
Lily Le <lmlt8180@gmail.com>
2024-09-29 18:54:13 -0400

    organize code

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,13 +1,15 @@
 // import styling
-import { BasicTabs } from "../../components/Tabs/Tabs.jsx";
 import "./AboutPage.scss";
-import ScrollAnimation from "react-animate-on-scroll";
 import "animate.css/animate.compat.css"
+
+// import components
+import { BasicTabs } from "../../components/Tabs/Tabs.jsx";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export function AboutPage() {
     return (
-        <ScrollAnimation animateIn="fadeInLeft" duration={2}>
-            <section className="about" id="about">
+        <section className="about" id="about">
+            <ScrollAnimation animateIn="fadeInLeft" duration={2}>
                 <h1 className="about__header">
                     About
                 </h1>
@@ -19,9 +21,9 @@ export function AboutPage() {
                     I also honed my ability to communicate effectively, manage high-stress situations,
                     and think critically—skills that seamlessly translate to software development.
                 </p>
-                
+
                 <BasicTabs />
-            </section>
-        </ScrollAnimation>
+            </ScrollAnimation>
+        </section>
     );
 };
