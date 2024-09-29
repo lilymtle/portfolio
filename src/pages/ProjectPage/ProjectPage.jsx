27c6a80c93597b15f6412327ece9d455c3a6dd1d
Lily Le <lmlt8180@gmail.com>
2024-09-29 19:04:45 -0400

    organize code

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,35 +1,32 @@
 // import styling
 import "./ProjectPage.scss";
-
-import { Carousel } from "../../components/Carousel/Carousel.jsx";
-import ScrollAnimation from "react-animate-on-scroll";
 import "animate.css/animate.compat.css"
 
+// import components
+import { Carousel } from "../../components/Carousel/Carousel.jsx";
+import ScrollAnimation from "react-animate-on-scroll";
 
 export function ProjectPage() {
 
-
-    const OPTIONS = {}
+const OPTIONS = {}
 const SLIDE_COUNT = 5
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
-
     return (
-        <ScrollAnimation animateIn="fadeInRight" duration={2}>
             <section className="projects" id="projects">
-                <h1 className="project__header">
-                    Projects
-                </h1>
+                <ScrollAnimation animateIn="fadeInRight" duration={2}>
+                    <h1 className="project__header">
+                        Projects
+                    </h1>
 
-                <p className="project__text">
-                    Hover over the images for more details.
-                </p>
+                    <p className="project__text">
+                        Hover over the images for more details.
+                    </p>
 
-                <section className="project__carousel">
-                    <Carousel slides={SLIDES} options={OPTIONS} />
-                </section>
-
+                    <section className="project__carousel">
+                        <Carousel slides={SLIDES} options={OPTIONS} />
+                    </section>
+                </ScrollAnimation>
             </section>
-        </ScrollAnimation>
-    )
-}
+    );
+};
