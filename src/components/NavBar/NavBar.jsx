43b884bdc6875cd3d3f3be832098f76484542559
Lily Le <lmlt8180@gmail.com>
2024-09-29 18:40:57 -0400

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,30 +10,17 @@ export function NavBar() {
             <ul className="nav-bar__list">
                 <li className="nav-bar__item">
                     <a className="nav-bar__link" href="#about">
-                        <p className="nav-bar__text">
-                            About
-                        </p>
+                        About
                     </a>
                 </li>
                 <li className="nav-bar__item">
                 <a className="nav-bar__link" href="#projects">
-                        <p className="nav-bar__text">
-                            Projects
-                        </p>
+                        Projects
                     </a>
                 </li>
                 <li className="nav-bar__item">
-                <a className="nav-bar__link" href="#work">
-                        <p className="nav-bar__text">
-                            Work
-                        </p>
-                    </a>
-                </li>
-                <li className="nav-bar__item">
-                    <Link className="nav-bar__link" to="/">
-                        <p className="nav-bar__btn">
-                            Resume
-                        </p>
+                    <Link className="nav-bar__btn" to="/">
+                        Resume
                     </Link>
                 </li>
             </ul>
