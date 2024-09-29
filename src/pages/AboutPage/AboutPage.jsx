69ed813595d865e5b4b1991705c4b84d62c5b277
Lily Le <lmlt8180@gmail.com>
2024-09-28 23:54:48 -0400

    add animation to pages

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,23 +1,27 @@
 // import styling
-import { BasicTabs } from "../../components/Tabs/Tabs";
+import { BasicTabs } from "../../components/Tabs/Tabs.jsx";
 import "./AboutPage.scss";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.compat.css"
 
 export function AboutPage() {
     return (
-        <section className="about" id="about">
-            <h2 className="about__subheader">
-                About
-            </h2>
+        <ScrollAnimation animateIn="fadeInLeft" duration={2}>
+            <section className="about" id="about">
+                <h2 className="about__subheader">
+                    About
+                </h2>
 
-            <p className="about__text">
-                As a former nurse transitioning to software engineering, I bring a unique 
-                perspective to tech development. My nursing experience has shaped my understanding
-                of user needs, allowing me to create empathetic, efficient software solutions.
-                I also honed my ability to communicate effectively, manage high-stress situations,
-                and think critically—skills that seamlessly translate to software development.
-            </p>
-            
-            <BasicTabs />
-        </section>
+                <p className="about__text">
+                    As a former nurse transitioning to software engineering, I bring a unique 
+                    perspective to tech development. My nursing experience has shaped my understanding
+                    of user needs, allowing me to create empathetic, efficient software solutions.
+                    I also honed my ability to communicate effectively, manage high-stress situations,
+                    and think critically—skills that seamlessly translate to software development.
+                </p>
+                
+                <BasicTabs />
+            </section>
+        </ScrollAnimation>
     );
 };
--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -2,6 +2,9 @@
 import "./ProjectPage.scss";
 
 import { Carousel } from "../../components/Carousel/Carousel.jsx";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.compat.css"
+
 
 export function ProjectPage() {
 
@@ -12,19 +15,21 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 
     return (
-        <section className="projects" id="projects">
-            <h2 className="project__subheader">
-                Projects
-            </h2>
+        <ScrollAnimation animateIn="fadeInRight" duration={2}>
+            <section className="projects" id="projects">
+                <h2 className="project__subheader">
+                    Projects
+                </h2>
 
-            <p className="project__text">
-                Hover over the images for more details.
-            </p>
+                <p className="project__text">
+                    Hover over the images for more details.
+                </p>
 
-            <section className="project__carousel">
-                <Carousel slides={SLIDES} options={OPTIONS} />
+                <section className="project__carousel">
+                    <Carousel slides={SLIDES} options={OPTIONS} />
+                </section>
+
             </section>
-
-        </section>
+        </ScrollAnimation>
     )
 }
