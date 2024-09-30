49d5ea11e329e0d64d8b1e359d510c5124fad5dd
Lily Le <lmlt8180@gmail.com>
2024-09-29 21:55:03 -0400

    delete commented out code

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,23 +1,11 @@
+// import styling
+import "./Modal.scss";
+
+// import components
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-
 import Button from '@mui/material/Button';
-
-import "./Modal.scss";
-
-
-// const style = {
-//     position: 'absolute',
-//     top: '50%',
-//     left: '50%',
-//     transform: 'translate(-50%, -50%)',
-//     width: 400,
-//     bgcolor: 'background.paper',
-//     border: '2px solid #000',
-//     boxShadow: 24,
-//     p: 4,
-// };
 
 export default function BasicModal({ open, handleClose, title, videoPath }) {
 
