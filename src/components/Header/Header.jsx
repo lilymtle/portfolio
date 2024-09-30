eb96a9f2968bc7fd49a3fc8925784c588b22b0d8
Lily Le <lmlt8180@gmail.com>
2024-09-30 16:31:29 -0400

    import logo and replace img src

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 // import styling
 import "./Header.scss";
 
+// import logo
+import logo from "../../assets/logos/portfolio-logo.png";
+
 // import components
 import { NavBar } from "../NavBar/NavBar";
 import { Link } from "react-router-dom";
@@ -11,7 +14,7 @@ export function Header() {
             <Link to="/">
                 <img
                 className="portfolio-logo"
-                src="/src/assets/logos/portfolio-logo.png"
+                src={logo}
                 alt="portfolio logo" />
             </Link>
 
