2eec874d6c8b0bb2a9c407f1813dd1316aa02833
Lily Le <lmlt8180@gmail.com>
2024-11-18 17:10:58 -0500

    update skills data to include tools and soft skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,10 +1,16 @@
-interface Skills {
+interface HardSkills {
     id: number;
     icon: string;
     alt: string;
 };
 
-export const techLanguages: Skills[] = [
+interface SoftSkills {
+    id: number;
+    emoji: string;
+    skill: string;
+}
+
+export const techLanguages: HardSkills[] = [
     {
         id: 1,
         icon: "/assets/icons/html.svg",
@@ -61,3 +67,104 @@ export const techLanguages: Skills[] = [
         alt: "MySQL"
     }
 ];
+
+export const techTools: HardSkills[] = [
+    {
+        id: 1,
+        icon: "/assets/icons/git.svg",
+        alt: "Git"
+    },
+    {
+        id: 2,
+        icon: "/assets/icons/github.svg",
+        alt: "GitHub"
+    },
+    {
+        id: 3,
+        icon: "/assets/icons/firebase.svg",
+        alt: "Firebase"
+    },
+    {
+        id: 4,
+        icon: "/assets/icons/postman.svg",
+        alt: "Postman"
+    },
+    {
+        id: 5,
+        icon: "/assets/icons/material-ui.svg",
+        alt: "Material UI"
+    }
+];
+
+export const softSkills: SoftSkills[] = [
+    {
+        id: 1,
+        emoji: "⭐️",
+        skill: "Leadership"
+    },
+    {
+        id: 2,
+        emoji: "💬",
+        skill: "Communication"
+    },
+    {
+        id: 3,
+        emoji: "🫶🏻",
+        skill: "Empathy"
+    },
+    {
+        id: 4,
+        emoji: "👯‍♀️",
+        skill: "Collaboration"
+    },
+    {
+        id: 5,
+        emoji: "💡",
+        skill: "Critical Thinking"
+    },
+    {
+        id: 6,
+        emoji: "👥💭",
+        skill: "User-centred Thinking"
+    },
+    {
+        id: 7,
+        emoji: "🗓️",
+        skill: "Organization"
+    },
+    {
+        id: 8,
+        emoji: "🔧",
+        skill: "Problen Solving"
+    },
+    {
+        id: 9,
+        emoji: "🏋🏻‍♀️",
+        skill: "Adaptability"
+    },
+    {
+        id: 10,
+        emoji: "📑",
+        skill: "Project Management"
+    },
+    {
+        id: 11,
+        emoji: "📚",
+        skill: "Education"
+    },
+    {
+        id: 12,
+        emoji: "🤹‍♂️",
+        skill: "Multi-tasking"
+    },
+    {
+        id: 13,
+        emoji: "📊",
+        skill: "Data Analysis"
+    },
+    {
+        id: 14,
+        emoji: "📝",
+        skill: "Documentation"
+    }
+]
