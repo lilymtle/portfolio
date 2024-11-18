a67eca783dd115dd352dc0bc1d9408c35d3b1a89
Lily Le <lmlt8180@gmail.com>
2024-11-17 19:37:02 -0500

    add font assets

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,8 @@ import githubLogo from "../../assets/logos/github-logo.svg";
 // import images
 import heroImage from "../../assets/images/hero-character.png";
 
-// import component
+// import components
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import { Link } from "react-router-dom";
 
 export function Hero() {
@@ -19,6 +20,26 @@ export function Hero() {
                     className="hero__character"
                     src={heroImage}
                     alt="illustration of a woman sitting on a beanbag with her hands in her pockets"
+                />
+
+                <div className="hero__content">
+                    <h1 className="hero__header">
+                        Hi, there!
+                    </h1>
+
+                    <p className="hero__text">
+                        My name is <span className="hero__text--lavender">Lily</span>, and it's so
+                        awesome to meet you.
+                    </p>
+
+                    <p className="hero__text hero__text--cta">
+                        Let's get in touch!
+                    </p>
+                </div>
+
+                <ArrowDownwardIcon 
+                    className="hero__arrow-icon"
+                    sx={{color: "#6962AD"}}
                 />
             </div>
         </section>
