c7180632852f4a67d421eb7e9ae38ba7a7fab3e3
Lily Le <lmlt8180@gmail.com>
2024-11-18 17:10:05 -0500

    change accordion jsx to tsx
    - update code to make it reuseable via props

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,33 @@
+// import styling
+import "./Accordion.scss";
+
+// import components
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { ReactNode } from "react";
+
+interface AccordionProps {
+    title: string;
+    children: ReactNode;
+}
+
+export default function AccordionBox({title, children}: AccordionProps): JSX.Element {
+    return (
+        <div>
+            <Accordion className="accordion">
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1-content"
+                    id="panel1-header"
+                >
+                    {title}
+                </AccordionSummary>
+                <AccordionDetails>
+                    {children}
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+}
