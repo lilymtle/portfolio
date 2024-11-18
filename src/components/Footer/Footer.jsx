0979ad1ee9f43d08f440cf739b6bcbbc0bff8c5a
Lily Le <lmlt8180@gmail.com>
2024-11-18 10:10:08 -0500

    update footer with text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,9 @@ import "./Footer.scss";
 export function Footer() {
     return (
         <footer>
-            Footer placeholder
+            <p className="footer__text">
+                © Crafted with 💜 by <span className="footer__text--bold">Lily</span>
+            </p>
         </footer>
     )
 };
