84bd8a9792d4727e4b26c79da56defd6b941ec82
Lily Le <lmlt8180@gmail.com>
2024-11-17 19:55:29 -0500

    delete old code

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,14 @@ import "./styles/global.scss";
 
 // import components
 import { BrowserRouter } from "react-router-dom";
-import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
 import { Hero } from "./components/Hero/Hero";
 
-// import pages
-import { AboutPage } from "./pages/AboutPage/AboutPage";
-import { ProjectPage } from "./pages/ProjectPage/ProjectPage";
 
 function App() {
 
   return (
     <BrowserRouter>
-        <Header />
-        <main>
-          <Hero />
-          <div className="app-container">
-            <AboutPage />
-            <ProjectPage />
-          </div>
-        </main>
-        <Footer />
+      <Hero />
     </BrowserRouter>
   )
 };
