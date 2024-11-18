9c639a759d9b631bd18af37986a88f8b42de2419
Lily Le <lmlt8180@gmail.com>
2024-11-17 21:11:59 -0500

    update header
    - import icon
    - add navigation links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
 // import styling
 import "./Header.scss";
 
+// import components
+import { Link } from "react-router-dom";
+
+// import image
+import catIcon from "../../assets/icons/cat-icon.gif";
+
 export function Header() {
     return (
-        <header>
-            Header placeholder
+        <header className="header">
+                <img
+                    className="header__icon"
+                    src={catIcon}
+                />
+
+            <nav className="navbar">
+                <Link className="navbar__link" to="/">
+                    About
+                </Link>
+
+                <Link className="navbar__link" to="/">
+                    Projects
+                </Link>
+
+                <Link className="navbar__link navbar__link--resume" to="/">
+                    Resume
+                </Link>
+            </nav>
         </header>
     )
 };
