6653e06e9198d977570beb25ea08fd0ccff4e454
Lily Le <lmlt8180@gmail.com>
2024-11-18 11:34:39 -0500

    create attributions data

---
+++ src/data/attributions.ts
@@ -0,0 +1,27 @@
+interface Attribution {
+    id: number;
+    name: string;
+    creator: string;
+    link: string;
+};
+
+export const attributions: Attribution[] = [
+    {
+        id: 1,
+        name: "Friendly Ones",
+        creator: "Diana Aguilar Ortiz",
+        link: "https://blush.design/collections/friendly-ones/friendly-ones"
+    },
+    {
+        id: 2,
+        name: "Tuxedo Pusheen Cat Avatar",
+        creator: "Lady-Pixel",
+        link: "https://www.deviantart.com/lady-pixel/art/Tuxedo-Pusheen-Cat-avatar-894886244"
+    },
+    {
+        id: 3,
+        name: "Abstract Simple Memphis Background",
+        creator: "rawpixel.com",
+        link: "https://www.freepik.com/free-vector/abstract-simple-memphis-background_16326606.htm"
+    }
+];
