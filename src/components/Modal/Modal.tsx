75195685f1150a2d4402970e0656d5cb6d1caf12
Lily Le <lmlt8180@gmail.com>
2024-11-18 11:34:06 -0500

    replace modal jsx with modal tsx

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,9 @@
 // import styling
 import "./Modal.scss";
 
+// import library
+import classNames from "classnames";
+
 // import components
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -8,7 +11,18 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
 
-export default function BasicModal({ open, handleClose, title, imagePath, imageAlt, videoPath }) {
+import { ReactNode } from "react";
+
+interface ModalProps {
+    open: boolean;
+    handleClose: ()=> void;
+    modalClassName?: string;
+    title: string;
+    children: ReactNode;
+    index?: ReactNode;
+}
+
+const BasicModal = ({ open, handleClose, modalClassName, title, children, index }: ModalProps): JSX.Element => {
 
     return (
         <div className="modal">
@@ -18,26 +32,18 @@ export default function BasicModal({ open, handleClose, title, imagePath, imageA
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box className="modal__box">
+                <Box className={classNames("modal__box", modalClassName)}>
                     <div className="modal__wrapper">
                         <Typography id="modal-modal-title" variant="h6" component="h2" className="modal__title">
                             {title}
                         </Typography>
                         <Button onClick={handleClose} className="modal__close-btn"><CloseIcon /></Button>
                     </div>
-
-                    {videoPath ? (
-                        <video controls width="100%">
-                            <source src={videoPath} type="video/mp4" />
-                        </video>
-                    ) : (
-                        <img
-                            className="modal__img"
-                            src={imagePath}
-                            alt={imageAlt} />
-                    )}
+                    {children}
                 </Box>
             </Modal>
         </div>
     );
 };
+
+export default BasicModal;
