80b6cef76d6e6b03f31057ce2c2be0282c1868c2
Lily Le <lmlt8180@gmail.com>
2024-11-19 14:57:59 -0500

    import project card component

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -3,6 +3,7 @@ import "./Projects.scss";
 
 // import data
 import { projects } from "../../data/projects";
+import { ProjectCard } from "../../components/Cards/ProjectCard/ProjectCard";
 
 export function ProjectsPage() {
     return (
@@ -10,6 +11,26 @@ export function ProjectsPage() {
             <h2 className="projects__header">
                 My Projects
             </h2>
+
+            <ul className="projects__list">
+                {projects.map((project, index) => (
+                    <li key={index} className="project__item">
+                        <ProjectCard 
+                            image={project.image}
+                            title={project.name} 
+                            description={project.description}
+                            features={project.features?.map((feature, featureIndex) => (
+                                <li key={featureIndex}>{feature}</li>
+                            )) || []}
+                            tools={project.tools.map((tool, toolIndex) => (
+                                <li key={toolIndex}>{tool}</li>
+                            ))}
+                            live={project.live}
+                            github={project.github}
+                        />
+                    </li>
+                ))}
+            </ul>
         </section>
     )
 };
