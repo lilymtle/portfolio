7f0be1da4503eafbc537da1a31ab7ff2188a3b32
Lily Le <lmlt8180@gmail.com>
2024-11-19 13:20:58 -0500

    create projects data file

---
+++ src/data/projects.ts
@@ -0,0 +1,186 @@
+interface Projects {
+    id: number;
+    name: string;
+    description: string;
+    role?: string;
+    features?: string[];
+    tools: string[];
+    image: string;
+    live?: string;
+    github?: string;
+};
+
+export const projects: Projects[] = [
+    {
+        id: 1,
+        name: "MoodCompass",
+        description: "MoodCompass is dedicated to mental health, providing a safe space for managing negative moods through education. Resources are available without an account, but users need to sign up to favorite them. A mood tracker and log will be added in the future.",
+        features: [
+            "User services (registration, login, and logout",
+            "User authentication and authorization",
+            "Protected routes",
+            "Favoriting and unfavoriting items",
+            "Dynamic data stored and retrieved from a database",
+            "Form validation"
+        ],
+        tools: [
+            "React",
+            "SASS",
+            "Node.js",
+            "MySQL",
+            "Express",
+            "Knex.js",
+            "Axios",
+            "Restful API Development",
+            "Firebase",
+            "Material UI",
+            "Git",
+            "GitHub"
+        ],
+        image: "/assets/images/mood-compass.png",
+        live: "https://mood-compass.vercel.app/",
+        github: "https://github.com/lilymtle/mood-compass"
+    },
+    {
+        id: 2,
+        name: "Koru",
+        description: "Koru",
+        role: "Full-Stack Developer",
+        tools: [
+            "Next.js",
+            "Tailwind CSS",
+            "JavaScript",
+            "TypeScript",
+            "Knex.js",
+            "MySQL",
+            "Material UI",
+            "Firebase",
+            "Git",
+            "GitHub"
+        ],
+        image: "/assets/images/mood-compass.png",
+        live: "https://mood-compass.vercel.app/",
+        github: "https://github.com/lilymtle/mood-compass"
+    },
+    {
+        id: 2,
+        name: "Portfolio",
+        description: "My personal porfolio.",
+        tools: [
+            "React",
+            "JavaScript",
+            "TypeScript",
+            "SASS",
+            "Material UI"
+        ],
+        image: "/assets/images/mood-compass.png",
+        github: "https://github.com/lilymtle/portfolio"
+    },
+    {
+        id: 3,
+        name: "TELUS What Your Preference Is",
+        description: "TELUS What Your Preference Is an interactive quiz new and existing TELUS users can use to receive their personalized TV bundle.The quiz is engaging and short in order to reduce option paralysis and decision fatigue.",
+        role: "Front-End Developer",
+        features: [
+            "Carousel hero banner",
+            "Personalized quiz"
+        ],
+        tools: [
+            "React",
+            "SASS",
+            "Axios",
+            "RESTFUL API Development",
+            "Git",
+            "GitHub",
+            "Agile (Scrum)",
+            "Jira"
+        ],
+        image: "/assets/images/mood-compass.png",
+        github: "https://github.com/lilymtle/portfolio"
+    },
+    {
+        id: 4,
+        name: "InStock",
+        description: "InStock is a fully responsive full-stack web application that delivers an inventory Management System for a Fortune 500 client. It features a list of the client's warehouses inventory items.",
+        role: "Full-Stack Developer, Scrum Master",
+        features: [
+            "Adding, modifying, and/or deleting a warehouse and/or inventory item",
+            "Dynamic data stored and retrieved from a database",
+            "Form validation"
+        ],
+        tools: [
+            "React",
+            "SASS",
+            "Node.js",
+            "MySQL",
+            "Express",
+            "Knex.js",
+            "Axios",
+            "RESTFUL API Development",
+            "Git",
+            "GitHub",
+            "Agile (Scrum)",
+            "Jira",
+            "Postman"
+        ],
+        image: "/assets/images/mood-compass.png",
+    },
+    {
+        id: 5,
+        name: "BrainFlix",
+        description: "BrainFlix is a fully responsive video streaming platform with its own API service.",
+        role: "Full-Stack Developer",
+        features: [
+            "Adding a video",
+            "Form validation"
+        ],
+        tools: [
+            "React",
+            "SASS",
+            "Axios",
+            "RESTful API Development",
+            "JSON",
+            "Git",
+            "GitHub",
+            "Postman"
+        ],
+        image: "/assets/images/mood-compass.png",
+    },
+    {
+        id: 6,
+        name: "BandSite",
+        description: "This is a fully responsive website for an up-and-coming band. It consists of two pages, the main page and shows page. The main page has information about the band and a photo gallery, whereas the shows page has a list of their upcoming shows.",
+        role: "Full-Stack Developer",
+        features: [
+            "Adding a comment",
+            "Form validation"
+        ],
+        tools: [
+            "HTML",
+            "SASS",
+            "JavaScript",
+            "Axios",
+            "RESTful API Integration",
+            "Git",
+            "GitHub"
+        ],
+        image: "/assets/images/mood-compass.png",
+    },
+    {
+        id: 7,
+        name: "CoffeeShop",
+        description: "Coffee Shop is a static multi-page web application that is broken down into two pages: landing page and menu page. The landing page has product information, and the menu page provides further details about the coffee shop's offerings.",
+        role: "Front-End Developer",
+        features: [
+            "Adding a comment",
+            "Form validation"
+        ],
+        tools: [
+            "HTML",
+            "SASS",
+            "Git",
+            "GitHub"
+        ],
+        image: "/assets/images/mood-compass.png",
+    }
+];
