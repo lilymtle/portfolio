3ffdf95e5db1a332b995ac966ee63140cfb98373
Lily Le <lmlt8180@gmail.com>
2024-11-20 00:34:57 -0500

    update validation
    - add error messages to schema
    - add type annotation for form data and errors
    - add validation
    - add submit and cancel handler

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,28 +3,107 @@ import "./Contact.scss";
 
 // import components
 import { TextField, InputAdornment } from "@mui/material";
-import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
+import PersonIcon from "@mui/icons-material/Person";
+import EmailIcon from "@mui/icons-material/Email";
 
 import Joi from "joi";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 const schema = Joi.object({
     name: Joi.string()
         .alphanum()
         .min(3)
         .max(30)
-        .required(),
+        .required()
+        .messages({
+            "string.empty": "Name is required",
+            "string.min": "Your name should be at least 3 characters long",
+            "string.max": "Your name should be at most 30 characters long"
+        }),
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: ["com", "ca", "net"]}})
-        .required,
+        .required()
+        .messages({
+            "string.empty": "Email is required",
+            "string.email": "Please enter a valid email address"
+        }),
     message: Joi.string()
         .alphanum()
         .min(3)
-        .max(30)
-        .required,
+        .max(300)
+        .required()
+        .messages({
+            "string.empty": "Message is required",
+            "string.min": "Message should be at least 3 characters long",
+            "string.max": "Message should be at most 300 characters long",
+        }),
 })
 
+interface FormData {
+    name: string;
+    email: string;
+    message: string;
+};
+
+interface Errors {
+    name: string;
+    email: string;
+    message: string;
+}
+
 export function ContactPage() {
+    const [formData, setFormData] = useState<FormData>({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    const [errors, setErrors] = useState<Errors>({
+        name: "",
+        email: "",
+        message: "",
+
+    });
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        const { error } = schema.validate(formData, { abortEarly: false});
+        
+        if (error) {
+            const newErrors: Errors = {
+                name: "",
+                email: "",
+                message: ""
+            };
+
+            error.details.map((detail) => {
+                newErrors[detail.path[0] as keyof Errors] = detail.message;
+            });
+
+            setErrors(newErrors);
+        } else {
+            setErrors({ name: "", email: "", message: "" });
+            setFormData({ name: "", email: "", message: ""});
+
+            alert("Form submitted!");
+        }
+    };
+
+    const handleCancel = (e: FormEvent) => {
+        e.preventDefault();
+        setErrors({ name: "", email: "", message: "" });
+        setFormData({ name: "", email: "", message: ""});
+    };
+
     return (
         <section id="contact" className="contact">
             <div className="container">
@@ -54,12 +133,20 @@ export function ContactPage() {
                             </p>
                         </div>
 
-                        <form className="contact__form">
+                        <form 
+                            className="contact__form"
+                            onSubmit={handleSubmit}
+                        >
                             <div className="input__wrapper">
                                 <TextField
+                                    name="name"
+                                    value={formData.name}
+                                    onChange={handleInputChange}
                                     className="input__field"
                                     label="name"
                                     variant="filled"
+                                    error={!!errors.name}
+                                    helperText={errors.name}
                                     slotProps={{
                                         input: {
                                             startAdornment: (
@@ -74,9 +161,14 @@ export function ContactPage() {
 
                             <div className="input__wrapper">
                                 <TextField
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleInputChange}
                                     className="input__field"
                                     label="email"
                                     variant="filled"
+                                    error={!!errors.email}
+                                    helperText={errors.email}
                                     slotProps={{
                                         input: {
                                             startAdornment: (
@@ -91,19 +183,24 @@ export function ContactPage() {
 
                             <div className="input__wrapper">
                                 <TextField
+                                    name="message"
+                                    value={formData.message}
+                                    onChange={handleInputChange}
                                     className="input__field"
                                     label="message"
                                     id="message"
                                     variant="filled"
+                                    error={!!errors.message}
+                                    helperText={errors.message}
                                     multiline
                                     rows={2}
                                 />
                             </div>
 
                             <div className="contact__buttons">
-                                <button className="button button--cancel">CANCEL</button>
+                                <button className="button button--cancel" type="reset" onClick={handleCancel}>CANCEL</button>
                                 
-                                <button className="button button--send">SEND</button>
+                                <button className="button button--send" type="submit">SEND</button>
                             </div>
                         </form>
                     </div>
