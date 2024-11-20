077a561346b22c71d52e38478b4244ce2896dd11
Lily Le <lmlt8180@gmail.com>
2024-11-20 01:50:04 -0500

    create custom type declaration file

---
+++ src/types/declarations.d.ts
@@ -0,0 +1,9 @@
+declare global {
+    namespace JSX {
+      interface IntrinsicElements {
+        'click-spark': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      }
+    }
+  }
+  
+  export {};  
