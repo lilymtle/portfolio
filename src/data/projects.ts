7667419b4290003595f7b6845c647c583b5a6b1e
Lily Le <lmlt8180@gmail.com>
2024-12-02 15:55:38 -0500

    updated project data
    - created variables to hold github url path and badge assets path
    - update badge asset path

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,28 +10,31 @@ interface Projects {
     notes?: string;
 };
 
+const githubPath = "https://www.github.com/lilymtle";
+const badgePath = "/assets/badges"
+
 export const projects: Projects[] = [
     {
         id: 1,
         name: "MoodCompass",
         description: "MoodCompass offers a safe space for managing negative moods through educational resources.",
         tools: [
-            "/assets/badges/react.svg",
-            "/assets/badges/nodejs.svg",
-            "/assets/badges/mysql.svg",
-            "/assets/badges/javascript.svg",
-            "/assets/badges/express.svg",
-            "/assets/badges/knex.png",
-            "/assets/badges/axios.png",
-            "/assets/badges/sass.svg",
-            "/assets/badges/firebase.svg",
-            "/assets/badges/material-ui.svg",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg"
+            `${badgePath}/react.svg`,
+            `${badgePath}/nodejs.svg`,
+            `${badgePath}/mysql.svg`,
+            `${badgePath}/javascript.svg`,
+            `${badgePath}/express.svg`,
+            `${badgePath}/knex.png`,
+            `${badgePath}/axios.png`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/firebase.svg`,
+            `${badgePath}/material-ui.svg`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`
         ],
         image: "/assets/images/mood-compass.png",
         live: "https://mood-compass.vercel.app/",
-        github: "https://github.com/lilymtle/mood-compass",
+        github: `${githubPath}/mood-compass`,
         notes: "MoodCompass is still a work in progress. Feel free to check the GitHub repo for updates."
     },
     {
@@ -39,15 +42,15 @@ export const projects: Projects[] = [
         name: "Koru",
         description: "Koru connects you to the Māori concept of growth and renewal, inviting daily reflections to foster gratitude and mindfulness.",
         tools: [
-            "/assets/badges/nextjs.svg",
-            "/assets/badges/typescript.svg",
-            "/assets/badges/mysql.svg",
-            "/assets/badges/knex.png",
-            "/assets/badges/tailwind.svg",
-            "/assets/badges/firebase.svg",
-            "/assets/badges/material-ui.svg",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg"
+            `${badgePath}/nextjs.svg`,
+            `${badgePath}/typescript.svg`,
+            `${badgePath}/mysql.svg`,
+            `${badgePath}/knex.png`,
+            `${badgePath}/tailwind.svg`,
+            `${badgePath}/firebase.svg`,
+            `${badgePath}/material-ui.svg`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`
         ],
         image: "/assets/images/koru.png",
         github: "https://github.com/lilymtle/koru",
@@ -58,12 +61,12 @@ export const projects: Projects[] = [
         name: "Portfolio",
         description: "My personal portfolio, offering a glimpse into who I am and the projects I've worked on. The thumbnail and preview display the previous version of my porfolio.",
         tools: [
-            "/assets/badges/react.svg",
-            "/assets/badges/typescript.svg",
-            "/assets/badges/sass.svg",
-            "/assets/badges/material-ui.svg",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg"
+            `${badgePath}/react.svg`,
+            `${badgePath}/typescript.svg`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/material-ui.svg`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`
         ],
         image: "/assets/images/portfolio.png",
         video: "/assets/videos/portfolio.mp4",
@@ -74,11 +77,11 @@ export const projects: Projects[] = [
         name: "CheerBot",
         description: "CheerBot is a positivity-driven Discord bot designed to spread good vibes and uplift your community.",
         tools: [
-            "./assets/badges/javascript.svg",
-            "./assets/badges/discordjs.png",
-            "/assets/badges/axios.png",
-            "/assets/badges/git.svg",
-            "assets/badges/github.svg"
+            `.${badgePath}/javascript.svg`,
+            `.${badgePath}/discordjs.png`,
+            `${badgePath}/axios.png`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`
         ],
         image: "/assets/images/cheerbot.png",
         video: "/assets/videos/cheerbot.mp4",
@@ -89,13 +92,13 @@ export const projects: Projects[] = [
         name: "TELUS",
         description: "TELUS What Your Preference Is an interactive quiz new and existing TELUS users can use to receive their personalized TV bundle.",
         tools: [
-            "/assets/badges/react.svg",
-            "/assets/badges/javascript.svg",
-            "/assets/badges/axios.png",
-            "/assets/badges/sass.svg",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg",
-            "/assets/badges/jira.svg"
+            `${badgePath}/react.svg`,
+            `${badgePath}/javascript.svg`,
+            `${badgePath}/axios.png`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`,
+            `${badgePath}/jira.svg`
         ],
         image: "/assets/images/telus.png",
         video: "/assets/videos/telus.mp4"
@@ -105,17 +108,17 @@ export const projects: Projects[] = [
         name: "InStock",
         description: "InStock is a fully responsive full-stack web app providing an inventory management system for a Fortune 500 client. It displays a list of warehouse inventory.",
         tools: [
-            "/assets/badges/react.svg",
-            "/assets/badges/nodejs.svg",
-            "/assets/badges/mysql.svg",
-            "/assets/badges/express.svg",
-            "/assets/badges/knex.png",
-            "/assets/badges/axios.png",
-            "/assets/badges/sass.svg",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg",
-            "/assets/badges/jira.svg",
-            "/assets/badges/postman.svg"
+            `${badgePath}/react.svg`,
+            `${badgePath}/nodejs.svg`,
+            `${badgePath}/mysql.svg`,
+            `${badgePath}/express.svg`,
+            `${badgePath}/knex.png`,
+            `${badgePath}/axios.png`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`,
+            `${badgePath}/jira.svg`,
+            `${badgePath}/postman.svg`
         ],
         image: "/assets/images/instock.png",
         video: "/assets/videos/instock.mp4",
@@ -126,13 +129,13 @@ export const projects: Projects[] = [
         name: "BrainFlix",
         description: "BrainFlix is a fully responsive video streaming platform with its own API service.",
         tools: [
-            "/assets/badges/react.svg",
-            "/assets/badges/axios.png",
-            "/assets/badges/sass.svg",
-            "/assets/badges/json.png",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg",
-            "/assets/badges/postman.svg"
+            `${badgePath}/react.svg`,
+            `${badgePath}/axios.png`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/json.png`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`,
+            `${badgePath}/postman.svg`
         ],
         image: "/assets/images/brainflix.png",
         video: "/assets/videos/brainflix.mp4",
@@ -144,12 +147,12 @@ export const projects: Projects[] = [
         name: "BandSite",
         description: "A fully responsive website for an emerging band, featuring two pages: the main page with band information and a photo gallery, and a shows page listing upcoming performances.",
         tools: [
-            "/assets/badges/html.svg",
-            "/assets/badges/sass.svg",
-            "/assets/badges/javascript.svg",
-            "/assets/badges/axios.png",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg",
+            `${badgePath}/html.svg`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/javascript.svg`,
+            `${badgePath}/axios.png`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`,
         ],
         image: "/assets/images/bandsite.png",
         video: "/assets/videos/bandsite.mp4",
@@ -160,10 +163,10 @@ export const projects: Projects[] = [
         name: "CoffeeShop",
         description: "CoffeeShop is a static, multi-page web app with two pages: a landing page featuring product information and a menu page detailing the shop's offerings.",
         tools: [
-            "/assets/badges/html.svg",
-            "/assets/badges/sass.svg",
-            "/assets/badges/git.svg",
-            "/assets/badges/github.svg",
+            `${badgePath}/html.svg`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`,
         ],
         image: "/assets/images/coffeeshop.png",
         video: "/assets/videos/coffeeshop.mp4",
@@ -174,13 +177,13 @@ export const projects: Projects[] = [
         name: "CINEMATICATS",
         description: "CINEMATICATs is a static web application that help users find movie recommendations with a cat companion.",
         tools: [
-            "../assets/badges/html.svg",
-            "../assets/badges/sass.svg",
-            "../assets/badges/javascript.svg",
-            "../assets/badges/axios.png",
-            "../assets/badges/git.svg",
-            "../assets/badges/github.svg",
-            "../assets/badges/figma.svg"
+            `${badgePath}/html.svg`,
+            `${badgePath}/sass.svg`,
+            `${badgePath}/javascript.svg`,
+            `${badgePath}/axios.png`,
+            `${badgePath}/git.svg`,
+            `${badgePath}/github.svg`,
+            `${badgePath}/figma.svg`
         ],
         image: "/assets/images/cinematicats.png",
         video: "/assets/videos/cinematicats.mp4"
