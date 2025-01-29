431f3aad747f03361ef6a9b02bd5639d70b710a3
Lily Le <lmlt8180@gmail.com>
2025-01-29 13:02:33 -0500

    update project info to include the verse

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,6 +18,18 @@ const videoPath = "/assets/videos";
 export const projects: Projects[] = [
     {
         id: 1,
+        name: "The Verse",
+        description: "As the intern and primary developer for the main site of The Verse, I collaborated with the designer, project manager, and founder to develop a website that showcases the company's mission and the innovative games they are creating. Currently, the project is focused on front-end development, with plans to implement a back end in the future.",
+        tools: [
+            `${badgePath}/react.svg`,
+            `${badgePath}/typescript.svg`,
+            `${badgePath}/sass.svg`
+        ],
+        image: `${imagePath}/the-verse.png`,
+        live: "https://versebuilding.com"
+    },
+    {
+        id: 2,
         name: "MoodCompass",
         description: "MoodCompass offers a safe space for managing negative moods through educational resources.",
         tools: [
@@ -40,7 +52,7 @@ export const projects: Projects[] = [
         notes: "MoodCompass is still a work in progress. Feel free to check the GitHub repo for updates."
     },
     {
-        id: 2,
+        id: 3,
         name: "Koru",
         description: "Koru connects you to the Māori concept of growth and renewal, inviting daily reflections to foster gratitude and mindfulness.",
         tools: [
@@ -59,7 +71,7 @@ export const projects: Projects[] = [
         notes: "Koru is still a work in progress. Feel free to check the GitHub repo for updates."
     },
     {
-        id: 3,
+        id: 4,
         name: "Portfolio",
         description: "My personal portfolio, offering a glimpse into who I am and the projects I've worked on. The thumbnail and preview display the previous version of my porfolio.",
         tools: [
@@ -75,7 +87,7 @@ export const projects: Projects[] = [
         github: `${githubPath}/portfolio`
     },
     {
-        id: 4,
+        id: 5,
         name: "CheerBot",
         description: "CheerBot is a positivity-driven Discord bot designed to spread good vibes and uplift your community.",
         tools: [
@@ -90,7 +102,7 @@ export const projects: Projects[] = [
         github: `${githubPath}/cheerbot`
     },
     {
-        id: 5,
+        id: 6,
         name: "TELUS",
         description: "TELUS What Your Preference Is an interactive quiz new and existing TELUS users can use to receive their personalized TV bundle.",
         tools: [
@@ -106,7 +118,7 @@ export const projects: Projects[] = [
         video: `${videoPath}/telus.mp4`
     },
     {
-        id: 6,
+        id: 7,
         name: "InStock",
         description: "InStock is a fully responsive full-stack web app providing an inventory management system for a Fortune 500 client. It displays a list of warehouse inventory.",
         tools: [
@@ -127,7 +139,7 @@ export const projects: Projects[] = [
         notes: "GitHub repo not publicly available per institution request."
     },
     {
-        id: 7,
+        id: 8,
         name: "BrainFlix",
         description: "BrainFlix is a fully responsive video streaming platform with its own API service.",
         tools: [
@@ -145,7 +157,7 @@ export const projects: Projects[] = [
 
     },
     {
-        id: 8,
+        id: 9,
         name: "BandSite",
         description: "A fully responsive website for an emerging band, featuring two pages: the main page with band information and a photo gallery, and a shows page listing upcoming performances.",
         tools: [
@@ -161,7 +173,7 @@ export const projects: Projects[] = [
         notes: "GitHub repo not publicly available per institution request."
     },
     {
-        id: 9,
+        id: 10,
         name: "CoffeeShop",
         description: "CoffeeShop is a static, multi-page web app with two pages: a landing page featuring product information and a menu page detailing the shop's offerings.",
         tools: [
@@ -175,7 +187,7 @@ export const projects: Projects[] = [
         notes: "GitHub repo not publicly available per institution request."
     },
     {
-        id: 10,
+        id: 11,
         name: "CINEMATICATS",
         description: "CINEMATICATs is a static web application that help users find movie recommendations with a cat companion.",
         tools: [
