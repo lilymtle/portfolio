c01230a5f344d4cb3eb0b735c06088e9630d76c2
Lily Le <lmlt8180@gmail.com>
2025-03-14 19:15:57 -0400

    create footer sitemap

--- src/components/BlogFooter/BlogFooter.tsx
+++ src/components/BlogFooter/BlogFooter.tsx
@@ -4,7 +4,91 @@ import "./BlogFooter.scss";
 export function BlogFooter() {
     return (
         <footer className="blog__footer">
-            Weeeeeee
+            <div className="blog__footer-container">
+                <h4 className="blog__footer-label">
+                    Blog
+                </h4>
+
+                <ul className="blog__footer-nav-list">
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">About</a>
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">Recent Posts</a>
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="blog__footer-container">
+                <h4 className="blog__footer-label">
+                    Topics
+                </h4>
+
+                <ul className="blog__footer-nav-list">
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">Books</a>
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">Health</a>
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">Lifestyle</a>
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">Technology</a>
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link"  href="/">Travel</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="blog__footer-container">
+                <h4 className="blog__footer-label">
+                    Resources
+                </h4>
+
+                <ul className="blog__footer-nav-list">
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">Courses</a>
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <a className="blog__footer-nav-link" href="/">FAQs</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="blog__footer-container">
+                <h4 className="blog__footer-label">
+                    Connect
+                </h4>
+
+                <ul className="blog__footer-nav-list--socials">
+                    <li className="blog__footer-nav-list-item">
+                        <img 
+                            className="blog__footer-icon"
+                            alt="LinkedIn Icon"
+                            src="/assets/logos/linkedin-logo.png" />
+                    </li>
+
+                    <li className="blog__footer-nav-list-item">
+                        <img 
+                            className="blog__footer-icon"
+                            alt="GitHub Icon"
+                            src="/assets/logos/github-logo.svg" />
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 };
