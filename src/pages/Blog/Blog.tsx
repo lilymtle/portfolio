82f4ec3ff72a61764b4ecd3efeb9817e6150c4bd
Lily Le <lmlt8180@gmail.com>
2025-03-14 18:47:19 -0400

    add helmet provider to blog

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,10 +1,16 @@
 // styling
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import "./Blog.scss";
+import { Header } from "../../components/Header/Header";
 
 export function BlogPage() {
     return (
-        <section className="blog">
-            Blog placeholder
-        </section>
+        <HelmetProvider>
+            <Helmet>
+                <title>
+                    Blog | Lily Le
+                </title>
+            </Helmet>
+        </HelmetProvider>
     );
 };
