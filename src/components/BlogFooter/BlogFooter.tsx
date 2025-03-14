e89b85a23e401e0a17565cfbc162fe78f8164490
Lily Le <lmlt8180@gmail.com>
2025-03-14 18:47:32 -0400

    create blog footer boilerplates

---
+++ src/components/BlogFooter/BlogFooter.tsx
@@ -0,0 +1,10 @@
+// styling
+import "./BlogFooter.scss";
+
+export function BlogFooter() {
+    return (
+        <footer className="blog__footer">
+            Weeeeeee
+        </footer>
+    );
+};
