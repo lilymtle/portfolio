39708a54588f94b9a96e51e5b5d44a122a0f9f60
Lily Le <lmlt8180@gmail.com>
2025-03-16 01:21:39 -0400

    add spotify embed

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -33,6 +33,8 @@ export function BlogPage() {
                     <h2 className="blog__home-subheader">
                         Playlist
                     </h2>
+
+                    <iframe src="https://open.spotify.com/embed/playlist/2noyZgg466AcWSAJo0HSEh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 </section>
             </main>
         </>
