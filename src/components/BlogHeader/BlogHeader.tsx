6167ba31e8ccc16224ba16ed2272d3299b5de1ad
Lily Le <lmlt8180@gmail.com>
2025-03-15 22:51:23 -0400

    update blog header
    - remove blog hero component
    - add transparent blog overlay
    - add blog logo
    - add header explore text

--- src/components/BlogHeader/BlogHeader.tsx
+++ src/components/BlogHeader/BlogHeader.tsx
@@ -2,38 +2,54 @@
 import "./BlogHeader.scss";
 
 /// components
-import { BlogHero } from "../BlogHero/BlogHero";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { Box } from "@mui/material";
 
 export function BlogHeader() {
     return (
         <header className="blog__header">
+
+            <div className="blog__overlay"></div>
+
             <div className="blog__container">
-                <BlogHero />
+                <img
+                    className="blog__logo"
+                    alt='Logo of a mountain with the text "Lilys Corner for Thoughts"'
+                    src="/assets/logos/blog-logo.png"
+                />
+
+                <p className="blog__header-text">
+                    Explore
+                </p>
             </div>
 
-            <nav className="blog__nav">
+            {/* <nav className="blog__nav">
                 <ul className="blog__nav-list">
                     <li className="blog__nav-list-item">
-                        Home
+                        <a className="blog__nav-link" href="/">Home</a>
                     </li>
 
                     <li className="blog__nav-list-item">
-                        Books
+                        <a className="blog__nav-link" href="/">Books</a>
                     </li>
 
                     <li className="blog__nav-list-item">
-                        Health
+                        <a className="blog__nav-link" href="/">Health</a>
                     </li>
 
                     <li className="blog__nav-list-item">
-                        Lifestyle
+                        <a className="blog__nav-link" href="/">Lifestyle</a>
                     </li>
 
                     <li className="blog__nav-list-item">
-                        Technology
+                        <a className="blog__nav-link" href="/">Travel</a>
+                    </li>
+
+                    <li className="blog__nav-list-item">
+                        <a className="blog__nav-link" href="/">Technology</a>
                     </li>
                 </ul>
-            </nav>
+            </nav> */}
         </header>
     );
 };
