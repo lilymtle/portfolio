1c6f680884bbd800d81a357bfa36e6054df4124a
Lily Le <lmlt8180@gmail.com>
2025-03-19 17:44:48 -0400

    add wrappers for tablet breakpoint styling

--- src/pages/BlogAbout/BlogAbout.tsx
+++ src/pages/BlogAbout/BlogAbout.tsx
@@ -14,72 +14,80 @@ export function BlogAbout() {
                 millions of thoughts running around in my head. 🫣
             </p>
             
+            <div className="blog__about-container">
+                <div className="blog__wrapper--subheader-image">
+                    <h2 className="blog__about-subheader">
+                        Hi, I'm Lily
+                    </h2>
+
+                    <img
+                        className="blog__about-image--me"
+                        src="/assets/images/me.png"
+                        alt="Photo of myself" />
+                </div>
+
+                <div className="blog__wrapper--text">
+                    <p className="blog__about-text">
+                        Nice to meet ya! I'm from Canada, where I live with my wonderful husband and
+                        two adorable cats. As a Leo, ENFJ, and Gryffindor, I like to think of myself as a
+                        burst of energy and sunshine. I'm passioante about reading, cooking, traveling,
+                        health & wellness, gaming, and so much more!
+                    </p>
+
+                    <p className="blog__about-text">
+                        I have a unique background that bridges healthcare and technology. Feel free
+                        to explore more about my professional journey by visiting my <a className="blog__about-link" href="/">portfolio</a>!
+                    </p>
+                </div>
+            </div>
+                            
             <h2 className="blog__about-subheader">
-                Hi, I'm Lily
+                The Why
             </h2>
 
             <div className="blog__about-container">
-                <img
-                    className="blog__about-image--me"
-                    src="/assets/images/me.png"
-                    alt="Photo of myself" />
+                <div className="blog__wrapper--image">
+                    <img 
+                        className="blog__about-image--illustration"
+                        src="/assets/images/blog-about-why-illustration.jpeg"
+                        alt="Illustration of a woman thinking while writing" />
+                </div>
 
-                <p className="blog__about-text">
-                    Nice to meet ya! I'm from Canada, where I live with my wonderful husband and
-                    two adorable cats. As a Leo, ENFJ, and Gryffindor, I like to think of myself as a
-                    burst of energy and sunshine. I'm passioante about reading, cooking, traveling,
-                    health & wellness, gaming, and so much more!
-                </p>
+                <div className="blog__wrapper--text">
+                    <p className="blog__about-text">
+                        I used to write a lot when I was younger. In fact, I took creative writing classes 
+                        and had a knack for short stories, poems, and fiction. But as time went by 
+                        <span className="blog__about-text--italics">*cough*</span> I became an adult with
+                        responsibilities <span className="blog__about-text--italics">*cough*</span>, I
+                        went from writing stories and poems to documentation, research papers, and articles.
+                    </p>
 
-                <p className="blog__about-text">
-                    I have a unique background that bridges healthcare and technology. Feel free
-                    to explore more about my professional journey by visiting my <a className="blog__about-link" href="/">portfolio</a>!
-                </p>
-            </div>
-                
-            <div className="blog__about-container">
-                <h3 className="blog__about-subheader">
-                    The Why
-                </h3>
+                    <p className="blog__about-text">
+                        My hopes with this space is to:
+                    </p>
 
-                <img 
-                    className="blog__about-image--illustration"
-                    src="/assets/images/blog-about-why-illustration.jpeg"
-                    alt="" />
+                    <ul className="blog__about-list">
+                        <li className="blog__about-list-item">
+                            Rekindle my love for and skills in writing
+                        </li>
 
-                <p className="blog__about-text">
-                    I used to write a lot when I was younger. In fact, I took creative writing classes 
-                    and had a knack for short stories, poems, and fiction. But as time went by 
-                    <span className="blog__about-text--italics">*cough*</span> I became an adult with
-                    responsibilities <span className="blog__about-text--italics">*cough*</span>, I
-                    went from writing stories and poems to documentation, research papers, and articles.
-                </p>
+                        <li className="blog__about-list-item">
+                            Rediscover my creativity
+                        </li>
 
-                <p className="blog__about-text">
-                    My hopes with this space is to:
-                </p>
+                        <li className="blog__about-list-item">
+                            Connect with others
+                        </li>
 
-                <ul className="blog__about-list">
-                    <li className="blog__about-list-item">
-                        Rekindle my love for and skills in writing
-                    </li>
+                        <li className="blog__about-list-item">
+                            Help others and myself
+                        </li>
 
-                    <li className="blog__about-list-item">
-                        Rediscover my creativity
-                    </li>
-
-                    <li className="blog__about-list-item">
-                        Connect with others
-                    </li>
-
-                    <li className="blog__about-list-item">
-                        Help others and myself
-                    </li>
-
-                    <li className="blog__about-list-item">
-                        Have fun
-                    </li>
-                </ul>
+                        <li className="blog__about-list-item">
+                            Have fun
+                        </li>
+                    </ul>
+                </div>
             </div>
 
             <h3 className="blog__about-subheader">
