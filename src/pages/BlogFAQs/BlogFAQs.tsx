4cb2248987cb922ed218190da6aa62d9b317e710
Lily Le <lmlt8180@gmail.com>
2025-03-19 18:00:40 -0400

    create blog faqs page and add boilerplate

---
+++ src/pages/BlogFAQs/BlogFAQs.tsx
@@ -0,0 +1,17 @@
+// styling
+import "./BlogFAQs.scss";
+
+export function BlogFAQs() {
+    return (
+        <section className="blog__faqs">
+            <h1 className="blog__faqs-header">
+                FAQs
+            </h1>
+
+            <p className="blog__text">
+                I have compiled a list of questions that I have been 
+                asked about—<span className="blog__text--italics">a lot</span>.
+            </p>
+        </section>
+    );
+};
