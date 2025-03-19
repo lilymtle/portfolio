0a288bb8b338e403746ff84db792e52e9dfb8e45
Lily Le <lmlt8180@gmail.com>
2025-03-19 19:16:48 -0400

    create blog faqs page and map through faqs data

--- src/pages/BlogFAQs/BlogFAQs.tsx
+++ src/pages/BlogFAQs/BlogFAQs.tsx
@@ -1,6 +1,12 @@
 // styling
 import "./BlogFAQs.scss";
 
+// component
+import AccordionBox from "../../components/Accordion/Accordion";
+
+// data
+import { blogFAQs, careerFAQs } from "../../data/faqs";
+
 export function BlogFAQs() {
     return (
         <section className="blog__faqs">
@@ -8,10 +14,45 @@ export function BlogFAQs() {
                 FAQs
             </h1>
 
-            <p className="blog__text">
+            <p className="blog__faqs-text">
                 I have compiled a list of questions that I have been 
-                asked about—<span className="blog__text--italics">a lot</span>.
+                asked about—<span className="blog__faqs-text--italics">frequently</span>. Check out
+                the categories below.
             </p>
+
+            <div className="blog__faqs-container">
+                <h2 className="blog__faqs-subheader">
+                    Blog
+                </h2>
+
+                {blogFAQs.map((blogFAQ, index) => (
+                    <AccordionBox
+                        title={blogFAQ.question}
+                        key={index}
+                    >
+                        <p className="blog__faqs-text blog__faqs-text--answer">
+                            {blogFAQ.answer}
+                        </p>
+                    </AccordionBox>
+                ))}
+            </div>
+
+            <div className="blog__faqs-container">
+                <h2 className="blog__faqs-subheader">
+                    Career
+                </h2>
+
+                {careerFAQs.map((careerFAQ, index) => (
+                    <AccordionBox
+                        title={careerFAQ.question}
+                        key={index}
+                    >
+                        <p className="blog__faqs-text blog__faqs-text--answer">
+                            {careerFAQ.answer}
+                        </p>
+                    </AccordionBox>
+                ))}
+            </div>
         </section>
     );
 };
