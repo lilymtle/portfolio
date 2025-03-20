f93e391369127ede16dd7846cc459629cf8c035c
Lily Le <lmlt8180@gmail.com>
2025-03-20 14:34:09 -0400

    create blog learning and contact page and styling with boilerplate code

---
+++ src/pages/BlogContact/BlogContact.tsx
@@ -0,0 +1,12 @@
+// styling
+import "./BlogContact.scss";
+
+export function BlogContactPage() {
+    return (
+        <section className="blog__contact">
+            <h1 className="blog__heading">
+                Send a Message
+            </h1>
+        </section>
+    );
+};
---
+++ src/pages/BlogLearning/BlogLearning.tsx
@@ -0,0 +1,20 @@
+// styling
+import "./BlogLearning.scss";
+
+export function BlogLearningPage() {
+    return (
+        <section className="blog__learning">
+            <h1 className="blog__heading">
+                Learning Resources
+            </h1>
+
+            <p className="blog__text">
+                Text 
+            </p>
+
+            <h2 className="blog__subheading">
+                Online Courses
+            </h2>
+        </section>
+    );
+};
