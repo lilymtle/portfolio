31b0e4d13ad985a08438cdbd302bb124e25600a3
Lily Le <lmlt8180@gmail.com>
2025-03-24 19:21:15 -0400

    create blog post card component

---
+++ src/components/BlogPostCard/BlogPostCard.tsx
@@ -0,0 +1,55 @@
+interface Category {
+    _id: string;
+    title: string;
+}
+
+interface BlogPostCardProps {
+    title: string;
+    image: string;
+    excerpt: string;
+    categories: Category[];
+    publishedAt: string;
+};
+
+
+export function BlogPostCard({ title, image, excerpt, categories, publishedAt }: BlogPostCardProps): JSX.Element {
+    return (
+        <div className="blog__post-card">
+            <div className="blog__post-card-image-wrapper">
+                <img
+                    className="blog__post-card-image"
+                    src={image}
+                    alt="Placeholder"
+                />
+            </div>
+
+            <div className="blog__post-card-content">
+                <div className="blog__post-card-title-description">
+                    <h2 className="blog__subheading">
+                        {title}
+                    </h2>
+
+                    <p className="blog__post-card-text">
+                        {excerpt} <a className="blog__post-card-link" href="/">read more</a>
+                    </p>
+                </div>
+
+                <div className="blog__post-card-footer">
+                    {categories.map((category) => (
+                        <p key={category._id} className="blog__post-card-category">
+                            {category.title}
+                        </p>
+                    ))}
+
+                    <p className="blog__post-card-divider">
+                        |
+                    </p>
+
+                    <p className="blog__post-card-date">
+                        {publishedAt}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
