d429c6554c1b8d0e6b4e794e0910ae0e976ffb4b
Lily Le <lmlt8180@gmail.com>
2025-03-24 20:35:12 -0400

    update blog post page
    - replace hardcoded cards with mapped blog post cards
    - add function to fetch all blog posts from sanity api
    - add function to sort posts
    - add pagination variables

--- src/pages/BlogPosts/BlogPosts.tsx
+++ src/pages/BlogPosts/BlogPosts.tsx
@@ -4,8 +4,49 @@ import "./BlogPosts.scss";
 // components
 import { BlogSubNav } from "../../components/BlogSubNav/BlogSubNav";
 import PaginationOutlined from "../../components/Pagination/Pagination";
+import { BlogPostCard } from "../../components/BlogPostCard/BlogPostCard";
+
+// utils & hooks
+import { getExcerpt } from "../../utils/getExcerpt";
+import { timeAgo } from "../../utils/timeAgo";
+import { useState, useEffect } from "react";
+
+// sanity
+import { getAllBlogPosts } from "../../sanity/fetchBlogPosts";
+import { urlFor } from "../../sanity/sanityImage";
+
 
 export function BlogPostsPage() {
+    const [posts, setPosts] = useState<any>(null);
+    const [sortOrder, setSortOrder] = useState<string>("latest");
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const postsPerPage: number = 5;
+
+    useEffect(() => {
+        const fetchBlogPosts = async () => {
+            try {
+                const data = await getAllBlogPosts();
+                if (!data) throw new Error("No posts found");
+                setPosts(data);
+            } catch (error) {
+                console.error("Error fetching posts:", error);
+            }
+        }
+        fetchBlogPosts();
+    }, []);
+
+    const sortedPosts = posts ? [...posts].sort((a, b) => {
+        return sortOrder === "latest"
+            ? new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+            : new Date(a.publishedAt).getTime() - new Date(b.publishedAt).getTime();
+    }) : [];
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = sortedPosts.slice(indexOfFirstPost, indexOfLastPost);
+    const totalPages = Math.ceil(sortedPosts.length / postsPerPage);
+
+
     return (
         <section className="blog__posts">
             <div className="blog__sub-nav-container">
@@ -22,7 +63,11 @@ export function BlogPostsPage() {
                 <div className="blog__sort-container">
                     <label className="blog__label">Sort by:</label>
 
-                    <select className="blog__sort-dropdown">
+                    <select
+                        className="blog__sort-dropdown"
+                        value={sortOrder}
+                        onChange={(e) => setSortOrder(e.target.value)}
+                    >
                         <option value="latest">Latest</option>
                         <option value="oldest">Oldest</option>
                     </select>
@@ -31,131 +76,30 @@ export function BlogPostsPage() {
 
             <section className="blog__posts-cards">
                 <ul className="blog__posts-list">
-                    <li className="blog__posts-item">
-                        <div className="blog__post-card-container">
-                            <div className="blog__post-card">
-                                <div className="blog__post-card-image-wrapper">
-                                    <img
-                                        className="blog__post-card-image"
-                                        src="/assets/images/hero-background.jpg"
-                                        alt="Placeholder"
-                                    />
-                                </div>
-
-                                <div className="blog__post-card-content">
-                                    <div className="blog__post-card-title-description">
-                                        <h2 className="blog__subheading">
-                                            Transitioning from Healthcare to Tech
-                                        </h2>
-
-                                        <p className="blog__post-card-text">
-                                            Healthcare and technology are such contrasting fields,
-                                            but they intersect in so many ways... <a className="blog__post-card-link" href="/">read more</a>
-                                        </p>
-                                    </div>
-
-                                    <div className="blog__post-card-footer">
-                                        <p className="blog__post-card-category">
-                                            Technology
-                                        </p>  
-
-                                        <p className="blog__post-card-divider">
-                                            |
-                                        </p>
-                                        
-                                        <p className="blog__post-card-date">
-                                            Yesterday
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="blog__posts-item">
-                        <div className="blog__post-card-container">
-                            <div className="blog__post-card">
-                                <div className="blog__post-card-image-wrapper">
-                                    <img
-                                        className="blog__post-card-image"
-                                        src="/assets/images/hero-background.jpg"
-                                        alt="Placeholder"
-                                    />
-                                </div>
-
-                                <div className="blog__post-card-content">
-                                    <div className="blog__post-card-title-description">
-                                        <h2 className="blog__subheading">
-                                            England & Scotland Honeymoon
-                                        </h2>
-
-                                        <p className="blog__post-card-text">
-                                            My husband and I went to the UK for our honeymoon,
-                                            and I left a piece of myself in Scotland... <a className="blog__post-card-link" href="/">read more</a>
-                                        </p>
-                                    </div>
-
-                                    <div className="blog__post-card-footer">
-                                        <p className="blog__post-card-category">
-                                            Travel
-                                        </p>  
-
-                                        <p className="blog__post-card-divider">
-                                            |
-                                        </p>
-                                        
-                                        <p className="blog__post-card-date">
-                                            2 days ago
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li className="blog__posts-item">
-                        <div className="blog__post-card-container">
-                            <div className="blog__post-card">
-                                <div className="blog__post-card-image-wrapper">
-                                    <img
-                                        className="blog__post-card-image"
-                                        src="/assets/images/hero-background.jpg"
-                                        alt="Placeholder"
-                                    />
-                                </div>
-
-                                <div className="blog__post-card-content">
-                                    <div className="blog__post-card-title-description">
-                                        <h2 className="blog__subheading">
-                                            Social Media Detox
-                                        </h2>
-
-                                        <p className="blog__post-card-text">
-                                            Have you ever wasted hours doom scrolling on social media? I watched a video recently that opened up my eyes on how our phone impacts... <a className="blog__post-card-link" href="/">read more</a>
-                                        </p>
-                                    </div>
-
-                                    <div className="blog__post-card-footer">
-                                        <p className="blog__post-card-category">
-                                            Lifestyle
-                                        </p>  
-
-                                        <p className="blog__post-card-divider">
-                                            |
-                                        </p>
-                                        
-                                        <p className="blog__post-card-date">
-                                            a week ago
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                    {currentPosts.length > 0 ? (
+                        currentPosts.map((post) => (
+                            <li key={post._id} className="blog__posts-list-item">
+                                <BlogPostCard
+                                    title={post.title}
+                                    image={urlFor(post.mainImage)}
+                                    excerpt={getExcerpt(post.body, 150)}
+                                    categories={post.categories}
+                                    publishedAt={timeAgo(post.publishedAt)}
+                                />
+                            </li>
+                        ))
+                    ) : (
+                        <p>Loading posts...</p>
+                    )}
                 </ul>
             </section>
-            
-            <PaginationOutlined />
+
+
+            <PaginationOutlined
+                count={totalPages}
+                page={currentPage}
+                onChange={setCurrentPage}
+            />
         </section>
     );
 };
