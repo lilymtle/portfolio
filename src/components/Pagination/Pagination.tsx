6db9661cfd76f94c51a64a5cd15a36866437e1c8
Lily Le <lmlt8180@gmail.com>
2025-03-24 20:35:25 -0400

    update pagination with passed props

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -5,11 +5,24 @@ import "./Pagination.scss";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
-export default function PaginationOutlined() {
+interface PaginationProps {
+    count: number;
+    page: number;
+    onChange: (page: number) => void;
+};
+
+export default function PaginationOutlined({ count, page, onChange }: PaginationProps) {
     return (
         <article className="pagination">
             <Stack spacing={2}>
-                <Pagination boundaryCount={1} siblingCount={0} count={10} variant="outlined" />
+                <Pagination 
+                    boundaryCount={1} 
+                    siblingCount={0}
+                    count={count} 
+                    page={page}
+                    onChange={(_, value) => onChange(value)}
+                    variant="outlined"
+                />
             </Stack>
         </article>
     );
