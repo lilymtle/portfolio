dcf22b4c4e66438bfdcfcb1e2b14ea1bae6f0b46
Lily Le <lmlt8180@gmail.com>
2025-03-26 14:21:16 -0400

    add resources data and create resource card

--- src/pages/BlogLearning/BlogLearning.tsx
+++ src/pages/BlogLearning/BlogLearning.tsx
@@ -1,9 +1,12 @@
 // styling
+import { learningResources } from "../../data/resources";
 import "./BlogLearning.scss";
+
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export function BlogLearningPage() {
     return (
-        <section className="blog__learning">
+        <section className="blog__resources">
             <h1 className="blog__heading">
                 Learning Resources
             </h1>
@@ -11,10 +14,23 @@ export function BlogLearningPage() {
             <p className="blog__text">
                 Text 
             </p>
+            
+            <ul className="blog__resources-list">
+                {learningResources.map((learningResource) => (
+                    <li className="blog__resources-list-item">
+                        <article className="blog__resource-card">
+                            <div className="blog__container--resource-subheading-text">
+                                <h2 className="blog__subheading blog__subheading--remove-top-margin">{learningResource.name}</h2>
+                                <p className="blog__text">{learningResource.website}</p>
+                            </div>
 
-            <h2 className="blog__subheading">
-                Online Courses
-            </h2>
+                            <div className="blog__container--resource-number">
+                                <a className="blog__resource-arrow-icon" href={learningResource.link}><ArrowForwardIcon /></a>
+                            </div>
+                        </article>
+                    </li>
+                ))}
+            </ul>
         </section>
     );
 };
