0d812c0457f15db47ec19dc04342dc874c99f47a
Lily Le <lmlt8180@gmail.com>
2025-03-26 21:10:13 -0400

    update blog page
    - update semantic tags
    - add divider component
    - add current items section

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -13,6 +13,8 @@ import { useEffect, useState } from "react";
 // sanity
 import { getLatestBlogPost } from "../../sanity/fetchBlogPosts";
 import { urlFor } from "../../sanity/sanityImage";
+import { Link } from "react-router-dom";
+import { Divider } from "@mui/material";
 
 export function BlogPage() {
     const [latestPost, setLatestPost] = useState<any>(null);
@@ -42,37 +44,70 @@ export function BlogPage() {
 
             <main>
                 <section id="welcome" className="blog__home">
-                    <h1 className="blog__home-header">
-                        Hi there 👋🏻
-                    </h1>
+                    <section className="blog__intro">
+                        <h1 className="blog__home-header">
+                            Hi there 👋🏻
+                        </h1>
 
-
-                    <p className="blog__text">
-                        After months of contemplation, I decided to create this space for me
-                        to pour my thoughts into. This wee corner of mine will include my
-                        ramblings on various topics, both technical and non-technical.
-                    </p>
+                        <p className="blog__text">
+                            After months of contemplation, I decided to create this space for me
+                            to pour my thoughts into. This wee corner of mine will include my
+                            ramblings on various topics, both technical and non-technical.
+                        </p>
+                    </section>
 
                     <h2 className="blog__home-subheader">
                         Latest Post
                     </h2>
 
-                    <article className="blog__post">
-                        {latestPost ? (
-                            <BlogPostCard
-                                title={latestPost.title}
-                                image={urlFor(latestPost.mainImage)}
-                                excerpt={getExcerpt(latestPost.body, 150)}
-                                categories={latestPost.categories}
-                                publishedAt={timeAgo(latestPost.publishedAt)}
-                            />
-                        ) : <p>Loading latest post...</p>}
-                    </article>
+                    <Link
+                        className="blog__latest-post-link"
+                        to={`/post/${latestPost?.slug?.current}`}
+                    >
+                            <article className="blog__post-card">
+                                {latestPost ? (
+                                    <BlogPostCard
+                                        title={latestPost.title}
+                                        image={urlFor(latestPost.mainImage)}
+                                        excerpt={getExcerpt(latestPost.body, 150)}
+                                        body={latestPost.body}
+                                        categories={latestPost.categories}
+                                        publishedAt={timeAgo(latestPost.publishedAt)}
+                                    />
+                                ) : <p>Loading latest post...</p>}
+                        </article>
+                    </Link>
 
-                    <h2 className="blog__home-subheader">
-                        Playlist
-                    </h2>
+                    <Divider className="blog__divider" />
 
+                    <section className="blog__current-activities">
+                        <h2 className="blog__home-subheader">
+                            Currently I am...
+                        </h2>
+                        
+                        <div className="blog__current-items">
+                            <article className="blog__current-item blog__current-item--game">
+                                <div className="blog__current-item-overlay"></div>
+                                <p className="blog__current-item-text">
+                                    Playing Baldur's Gate 3
+                                </p>
+                            </article>
+
+                            <article className="blog__current-item blog__current-item--show">
+                                <div className="blog__current-item-overlay"></div>
+                                <p className="blog__current-item-text">
+                                    Watching Dandadan
+                                </p>
+                            </article>
+
+                            <article className="blog__current-item blog__current-item--book">
+                                <div className="blog__current-item-overlay"></div>
+                                <p className="blog__current-item-text">
+                                    Reading Kitchen by Yoshimoto, Banana
+                                </p>
+                            </article>
+                        </div>
+                    </section>
                 </section>
             </main>
         </>
