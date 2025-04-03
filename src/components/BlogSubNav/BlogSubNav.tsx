6cbb794d0de64d464e3ea1801f06f0fd6513a559
Lily Le <lmlt8180@gmail.com>
2025-04-02 23:30:48 -0400

    replace hardcoded links with dynamic rendered links

--- src/components/BlogSubNav/BlogSubNav.tsx
+++ src/components/BlogSubNav/BlogSubNav.tsx
@@ -1,29 +1,43 @@
 // styling
 import "./BlogSubNav.scss";
 
-export function BlogSubNav() {
+import { useEffect, useState } from "react";
+import { sanityClient } from "../../sanity/sanity";
+import { Link } from "react-router-dom";
+
+export function BlogSubNav(): JSX.Element {
+const [categories, setCategories] = useState<string[]>([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const query = `*[_type == "category"]{ title}`;
+                const data = await sanityClient.fetch(query);
+                const categoryTitles = data.map((category: any) => category.title);
+                setCategories(categoryTitles);
+            } catch (error) {
+                console.error("Error fetching categories:", error);
+            };
+        };
+        fetchCategories();
+        console.log(categories);
+    }, []);
+
     return (
         <nav className="blog__sub-nav">
+            <p className="blog__sub-nav-label">Browse:</p>
+
             <ul className="blog__sub-nav-list">
-                <li className="blog__sub-nav-list-item">
-                    <a className="blog__sub-nav-link" href="/">Books</a>
-                </li>
-
-                <li className="blog__sub-nav-list-item">
-                    <a className="blog__sub-nav-link" href="/">Health</a>
-                </li>
-
-                <li className="blog__sub-nav-list-item">
-                    <a className="blog__sub-nav-link" href="/">Lifestyle</a>
-                </li>
-
-                <li className="blog__sub-nav-list-item">
-                    <a className="blog__sub-nav-link" href="/">Technology</a>
-                </li>
-
-                <li className="blog__sub-nav-list-item">
-                    <a className="blog__sub-nav-link" href="/">Travel</a>
-                </li>
+                {categories.map((category) => (
+                    <li key={category} className="blog__sub-nav-list-item">
+                        <Link
+                            className="blog__sub-nav-link"
+                            to={`/blog/category/${category}#top`}
+                        >
+                            {category}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </nav>
     );
