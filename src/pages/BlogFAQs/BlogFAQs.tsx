ec48b2d44532184c7631486033f325b80f7dee69
Lily Le <lmlt8180@gmail.com>
2025-04-02 23:29:11 -0400

    update blog faqs page helmet title

--- src/pages/BlogFAQs/BlogFAQs.tsx
+++ src/pages/BlogFAQs/BlogFAQs.tsx
@@ -7,50 +7,62 @@ import AccordionBox from "../../components/Accordion/Accordion";
 // data
 import { blogFAQs, careerFAQs } from "../../data/faqs";
 
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 export function BlogFAQs() {
     return (
-        <section className="blog__faqs">
-            <h1 className="blog__faqs-header">
-                FAQs
-            </h1>
+        <>
+            <HelmetProvider>
+                <Helmet>
+                    <title>
+                        Blog | FAQs
+                    </title>
+                </Helmet>
+            </HelmetProvider>
 
-            <p className="blog__faqs-text">
-                If you have any questions that aren't answered here, you can contact me here {/* add link later */}.
-            </p>
+            <section className="blog__faqs">
+                <h1 className="blog__faqs-header">
+                    FAQs
+                </h1>
 
-            <div className="blog__faqs-container">
-                <h2 className="blog__faqs-subheader">
-                    Blog
-                </h2>
+                <p className="blog__faqs-text">
+                    If you have any questions that aren't answered here, you can contact me here {/* add link later */}.
+                </p>
 
-                {blogFAQs.map((blogFAQ, index) => (
-                    <AccordionBox
-                        title={blogFAQ.question}
-                        key={index}
-                    >
-                        <p className="blog__faqs-text blog__faqs-text--answer">
-                            {blogFAQ.answer}
-                        </p>
-                    </AccordionBox>
-                ))}
-            </div>
+                <div className="blog__faqs-container">
+                    <h2 className="blog__faqs-subheader">
+                        Blog
+                    </h2>
 
-            <div className="blog__faqs-container">
-                <h2 className="blog__faqs-subheader">
-                    Career
-                </h2>
+                    {blogFAQs.map((blogFAQ, index) => (
+                        <AccordionBox
+                            title={blogFAQ.question}
+                            key={index}
+                        >
+                            <p className="blog__faqs-text blog__faqs-text--answer">
+                                {blogFAQ.answer}
+                            </p>
+                        </AccordionBox>
+                    ))}
+                </div>
 
-                {careerFAQs.map((careerFAQ, index) => (
-                    <AccordionBox
-                        title={careerFAQ.question}
-                        key={index}
-                    >
-                        <p className="blog__faqs-text blog__faqs-text--answer">
-                            {careerFAQ.answer}
-                        </p>
-                    </AccordionBox>
-                ))}
-            </div>
-        </section>
+                <div className="blog__faqs-container">
+                    <h2 className="blog__faqs-subheader">
+                        Career
+                    </h2>
+
+                    {careerFAQs.map((careerFAQ, index) => (
+                        <AccordionBox
+                            title={careerFAQ.question}
+                            key={index}
+                        >
+                            <p className="blog__faqs-text blog__faqs-text--answer">
+                                {careerFAQ.answer}
+                            </p>
+                        </AccordionBox>
+                    ))}
+                </div>
+            </section>
+        </>
     );
 };
