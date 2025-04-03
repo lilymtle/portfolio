7d7fbff44abe764e924efeed9967a0c82de0aa29
Lily Le <lmlt8180@gmail.com>
2025-04-02 23:32:20 -0400

    update blog header
    - replace hardcoded links with mapped links
    - replace explore link with spotify section

--- src/components/BlogHeader/BlogHeader.tsx
+++ src/components/BlogHeader/BlogHeader.tsx
@@ -4,7 +4,49 @@ import "./BlogHeader.scss";
 // components
 import { BlogMenu } from "../BlogMenu/BlogMenu";
 
-export function BlogHeader() {
+import { Link } from "react-router-dom";
+
+interface HeaderLink {
+    id: number;
+    label: string;
+    url: string;
+};
+
+export function BlogHeader(): JSX.Element {
+
+    const headerLinks: HeaderLink[] = [
+        {
+            id: 1,
+            label: "Home",
+            url: "/blog"
+        },
+        {
+            id: 2,
+            label: "About",
+            url: "/blog/about"
+        },
+        {
+            id: 3,
+            label: "Posts",
+            url: "/blog/posts"
+        },
+        {
+            id: 4,
+            label: "FAQs",
+            url: "/blog/faqs"
+        },
+        {
+            id: 5,
+            label: "Contact",
+            url: "/blog/contact"
+        },
+        {
+            id: 6,
+            label: "Portfolio",
+            url: "/"
+        }
+    ];
+
     return (
         <header className="blog__header">
 
@@ -18,25 +60,19 @@ export function BlogHeader() {
                     {/* navigation for tablet and desktop */}
                     <nav className="blog__nav-full">
                         <ul className="blog__nav-list">
-                            <li className="blog__nav-list-item">
-                                <a className="blog__nav-link" href="/blog/about">About</a>
-                            </li>
-
-                            <li className="blog__nav-list-item">
-                                <a className="blog__nav-link" href="/blog/posts">Posts</a>
-                            </li>
-
-                            <li className="blog__nav-list-item">
-                                <a className="blog__nav-link" href="/blog/about">FAQs</a>
-                            </li>
-
-                            <li className="blog__nav-list-item">
-                                <a className="blog__nav-link" href="/blog/contact">Contact</a>
-                            </li>
-
-                            <li className="blog__nav-list-item">
-                                <a className="blog__nav-link" href="/">Portfolio</a>
-                            </li>
+                            {headerLinks.map((link, index) => (
+                                <li key={link.id} className="blog__nav-list-item">
+                                    {index === headerLinks.length - 1 ? (
+                                        <Link className="blog__nav-link" to={link.url} target="_blank">
+                                            {link.label}
+                                        </Link>
+                                    ) : (
+                                        <Link className="blog__nav-link" to={link.url}>
+                                            {link.label}
+                                        </Link>
+                                    )}
+                                </li>
+                            ))}
                         </ul>
                     </nav>
                 </div>
@@ -51,9 +87,13 @@ export function BlogHeader() {
                     </a>
                 </div>
 
-                <a className="blog__header-link" href="#welcome">
-                    Explore
-                </a>
+                <div className="blog__wrapper-spotify">
+                    <p className="spotify-text">
+                        Now Playing
+                    </p>
+
+                    <iframe className="spotify" src="https://open.spotify.com/embed/track/7f85n3GoES8ODzpXmyzRpl?utm_source=generator&theme=0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </div>
             </div>
         </header>
     );
