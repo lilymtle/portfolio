3036578b88a569c191fa92eba60ad77a056bd461
Lily Le <lmlt8180@gmail.com>
2025-04-02 23:35:49 -0400

    update blog post card
    - store location and query params in link

--- src/components/BlogPostCard/BlogPostCard.tsx
+++ src/components/BlogPostCard/BlogPostCard.tsx
@@ -1,5 +1,5 @@
 // styling
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./BlogPostCard.scss";
 
 interface Category {
@@ -18,51 +18,58 @@ export interface BlogPostCard {
 };
 
 
-export function BlogPostCard({ title, image, excerpt, body, categories, publishedAt, slug}: BlogPostCard): JSX.Element {
+export function BlogPostCard({ title, image, excerpt, body, categories, publishedAt, slug }: BlogPostCard): JSX.Element {
+    const location = useLocation();
+
     return (
-        <Link className="blog__post-card-link" to={`/blog/post/${slug}`} state={{ title, image, body, categories, publishedAt }}>
-        <div className="blog__post-card-container">
-            <div className="blog__post-card">
-                <div className="blog__post-card-image-wrapper">
-                    <img
-                        className="blog__post-card-image"
-                        src={image}
-                        alt="Placeholder"
-                    />
-                </div>
-
-                <div className="blog__post-card-content">
-                    <div className="blog__post-card-title-description">
-                        <h2 className="blog__subheading">
-                            {title}
-                        </h2>
-
-                        <p className="blog__post-card-text">
-                            {excerpt}
-                        </p>
+        <Link 
+            className="blog__post-card-link" 
+            to={`/blog/post/${slug}`} 
+            state={{ title, image, body, categories, publishedAt, from: location.pathname + location.search }}
+        >
+            <div className="blog__post-card-container">
+                <div className="blog__post-card">
+                    <div className="blog__post-card-image-wrapper">
+                        <img
+                            className="blog__post-card-image"
+                            src={image}
+                            alt="Placeholder"
+                            loading="lazy"
+                        />
                     </div>
 
-                    <div className="blog__post-card-footer">
-                        {categories.map((category) => (
-                            <Link
-                                className="blog__post-card-category"
-                                to={`/blog/category/${category.title}`}
-                            >
-                                {category.title}
-                            </Link>
-                        ))}
+                    <div className="blog__post-card-content">
+                        <div className="blog__post-card-title-description">
+                            <h2 className="blog__subheading">
+                                {title}
+                            </h2>
 
-                        <p className="blog__post-card-divider">
-                            |
-                        </p>
+                            <p className="blog__post-card-text">
+                                {excerpt}
+                            </p>
+                        </div>
 
-                        <p className="blog__post-card-date">
-                            {publishedAt}
-                        </p>
+                        <div className="blog__post-card-footer">
+                            {categories.map((category) => (
+                                <Link
+                                    className="blog__post-card-category"
+                                    to={`/blog/category/${category.title}`}
+                                >
+                                    {category.title}
+                                </Link>
+                            ))}
+
+                            <p className="blog__post-card-divider">
+                                |
+                            </p>
+
+                            <p className="blog__post-card-date">
+                                {publishedAt}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </Link>
     );
 };
